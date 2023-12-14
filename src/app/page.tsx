0c8035d4cfb1d040f0c3c64aaba0986ce55879f3
Edgar Sandoval <isandoval.sed@gmail.com>
2023-12-14 17:26:55 -0600

    Created basic /login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,37 @@
+"use client";
+
+import { Box, Flex, Text } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/next-js";
+
 function Home() {
   return (
-    <main>
-      <h1>Cheaf Challenge</h1>
-    </main>
+    <Box
+      as="main"
+      h="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      bgColor="green.700"
+    >
+      <Flex gap="2rem" flexDir="column" alignItems="center">
+        <Text as="h1" fontSize="6xl" color="yellow.200">
+          Cheaf Challenge
+        </Text>
+        <Link
+          href="/login"
+          bgColor="yellow.100"
+          w="fit-content"
+          p="1rem 2rem"
+          borderRadius="0.875rem"
+          _hover={{
+            bgColor: "yellow.200",
+            textDecoration: "none",
+          }}
+        >
+          Ir a la página de login
+        </Link>
+      </Flex>
+    </Box>
   );
 }
 
