a5b6ffc28d0e2273c2ea0fab47996263080cb9ce
Edgar Sandoval <isandoval.sed@gmail.com>
2023-12-14 17:26:10 -0600

    Chakra-UI setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,10 +10,16 @@ export const metadata: Metadata = {
     "Cheaf busca acabar con el desperdicio de alimentos en tiendas y restaurantes",
 };
 
-function RootLayout({ children }: { children: React.ReactNode }) {
+function App({ children }: { children: React.ReactNode }): JSX.Element {
+  return <ChakraProvider>{children}</ChakraProvider>;
+}
+
+function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <App>{children}</App>
+      </body>
     </html>
   );
 }
